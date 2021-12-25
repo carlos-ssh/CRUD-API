a2258883639d68a94c0489a5503df7cae4e2b65f
Carlos <carlos.anezeh@gmail.com>
2021-12-25 13:24:07 -0600

    create a scaffold for todo.

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,19 +1,16 @@
 class TodosController < ApplicationController
   before_action :set_todo, only: [:show, :update, :destroy]
 
-  # GET /todos
   def index
     @todos = Todo.all
 
     render json: @todos
   end
 
-  # GET /todos/1
   def show
     render json: @todo
   end
 
-  # POST /todos
   def create
     @todo = Todo.new(todo_params)
 
@@ -24,7 +21,6 @@ class TodosController < ApplicationController
     end
   end
 
-  # PATCH/PUT /todos/1
   def update
     if @todo.update(todo_params)
       render json: @todo
@@ -33,18 +29,15 @@ class TodosController < ApplicationController
     end
   end
 
-  # DELETE /todos/1
   def destroy
     @todo.destroy
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_todo
       @todo = Todo.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def todo_params
       params.require(:todo).permit(:task)
     end
